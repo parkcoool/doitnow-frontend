a6f883d753ba9d13d4f15d202ad4d0a5582995bf
parkcoool <yickle1345@naver.com>
2024-02-23 19:20:24 +0900

    Chore: Create routers and pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import router from "routers";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
