f1122bcadc843b46f6d1ce4a70bf9c3914de88dc
parkcoool <yickle1345@naver.com>
2024-02-24 15:13:04 +0900

    Feat: Add properties for accessToken in `useSessionStore`

--- src/contexts/useSessionStore.ts
+++ src/contexts/useSessionStore.ts
@@ -4,16 +4,20 @@ import type { User } from "user";
 
 interface SessionValues {
   user: User | null;
+  accessToken: string | null;
 }
 
 interface SessionMethods {
   setUser: (user: User | null) => void;
+  setAccessToken: (accessToken: string | null) => void;
 }
 
 const useSessionStore = create<SessionValues & SessionMethods>((set) => ({
   user: null,
+  accessToken: null,
 
   setUser: (user) => set({ user }),
+  setAccessToken: (accessToken) => set({ accessToken }),
 }));
 
 export default useSessionStore;
