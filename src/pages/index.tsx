c89c6c402971d3bc1e3f43b9dd3ef6a267e6027e
parkcoool <yickle1345@naver.com>
2024-03-10 17:15:08 +0900

    Refactor: Remove `usePreferenceStore` context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
-import usePreferenceStore from "contexts/usePreferenceStore";
-
 import theme from "styles/theme";
 
 export default function App() {
-  const preference = usePreferenceStore();
-
   return (
     <>
       <CssBaseline />
