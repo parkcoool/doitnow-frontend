7f7a4f570df4adb393383bf1c3cb24c5c88f8d60
parkcoool <yickle1345@naver.com>
2024-03-24 14:38:54 +0900

    Feat(friend): Add ToggleButton

---
+++ src/pages/main/friend/components/FriendToggleButton.tsx
@@ -0,0 +1,68 @@
+/** @jsxImportSource @emotion/react */
+import { Typography, ToggleButton } from "@mui/material";
+
+import Avatar from "components/common/Avatar";
+import DeferredSkeleton from "components/common/DeferredSkeleton";
+
+import type { ToggleButtonProps } from "@mui/material";
+import type { SmallProfile } from "user";
+
+interface FriendProps extends ToggleButtonProps {
+  profile?: SmallProfile;
+}
+
+export default function FriendToggleButton({ profile, ...props }: FriendProps) {
+  return (
+    <ToggleButton
+      {...props}
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "8px",
+        padding: "0 8px",
+        boxSizing: "border-box",
+        minWidth: "80px",
+        maxWidth: "80px",
+        height: "100px",
+      }}
+      disabled={profile === undefined}
+    >
+      {/* 프로필 사진 */}
+      <div>
+        <Avatar
+          profileImage={profile?.profileImage}
+          name={profile?.name}
+          username={profile?.username}
+          css={{
+            width: "50px",
+            height: "50px",
+          }}
+        />
+      </div>
+
+      {/* 사용자 이름 */}
+      <div
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <Typography
+          css={{
+            width: "100%",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+          }}
+          fontWeight={600}
+          fontSize="12px"
+        >
+          {profile ? profile.username : <DeferredSkeleton width="100%" />}
+        </Typography>
+      </div>
+    </ToggleButton>
+  );
+}
--- src/pages/main/friend/index.tsx
+++ src/pages/main/friend/index.tsx
@@ -1,16 +1,12 @@
 /** @jsxImportSource @emotion/react */
 
 import React from "react";
-import InfiniteScroll from "react-infinite-scroller";
 
-import Typography from "@mui/material/Typography";
-import { CircularProgress } from "@mui/material";
-
+import { ToggleButtonGroup, toggleButtonGroupClasses } from "@mui/material";
 import getFriends from "apis/getFriends";
-
 import useSessionStore from "contexts/useSessionStore";
 
-import FriendComponent from "./components/Friend";
+import FriendComponent from "./components/FriendToggleButton";
 
 import type { SmallProfile } from "user";
 
@@ -19,17 +15,15 @@ export default function Friend() {
 
   const [initialLoading, setInitialLoading] = React.useState(true);
   const [moreLoading, setMoreLoading] = React.useState(false);
+
   const [hasMore, setHasMore] = React.useState(true);
   const [friends, setFriends] = React.useState<SmallProfile[]>([]);
-  const [expand, setExpand] = React.useState<number[]>([]);
+
+  const [selectedFriend, setSelectedFriend] = React.useState<SmallProfile | null>(null);
 
   React.useEffect(() => {
     handleInitialLoad();
   }, [session]);
-
-  function handleExpand(id: number) {
-    setExpand([id]);
-  }
 
   // 친구 불러오기
   async function loadFriends() {
@@ -56,71 +50,54 @@ export default function Friend() {
     setInitialLoading(false);
   }
 
+  function handleSelectFriend(event: React.MouseEvent<HTMLElement>, id: number | null) {
+    if (id === null) {
+      setSelectedFriend(null);
+      return;
+    }
+
+    const friend = friends.find((friend) => friend.id === id);
+    if (friend === undefined) return;
+
+    setSelectedFriend(friend);
+  }
+
   return (
     <>
-      <div>
+      <ToggleButtonGroup
+        value={selectedFriend?.id}
+        onChange={handleSelectFriend}
+        exclusive
+        css={{
+          display: "flex",
+          flexDirection: "row",
+          padding: "8px",
+          overflowX: "auto",
+          gap: "4px",
+          [`& .${toggleButtonGroupClasses.grouped}`]: {
+            border: 0,
+            borderRadius: "4px",
+            [`&.${toggleButtonGroupClasses.disabled}`]: {
+              border: 0,
+            },
+          },
+        }}
+      >
         {/* 로드 중일 때 */}
         {initialLoading && (
           <>
-            <FriendComponent />
-            <FriendComponent />
-            <FriendComponent />
-            <FriendComponent />
-            <FriendComponent />
+            <FriendComponent value={0} />
+            <FriendComponent value={0} />
+            <FriendComponent value={0} />
           </>
         )}
 
+        {/* TODO: infinite scroll */}
         {/* 친구가 있을 때 */}
-        {!initialLoading && friends.length > 0 && (
-          <InfiniteScroll
-            css={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "8px",
-            }}
-            loadMore={handleMoreLoad}
-            hasMore={hasMore}
-            useWindow
-            loader={
-              <div
-                key={0}
-                css={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  padding: "16px 0",
-                }}
-              >
-                <CircularProgress />
-              </div>
-            }
-          >
-            {friends.map((friend) => (
-              <FriendComponent
-                key={friend.id}
-                profile={friend}
-                expand={expand.includes(friend.id) ?? false}
-                onExpand={() => handleExpand(friend.id)}
-                onCollapse={() => setExpand([])}
-              />
-            ))}
-          </InfiniteScroll>
-        )}
-
-        {/* 친구가 없을 때 */}
-        {!initialLoading && friends.length === 0 && (
-          <div
-            css={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: "300px",
-            }}
-          >
-            <Typography>친구가 없어요.</Typography>
-          </div>
-        )}
-      </div>
+        {!initialLoading &&
+          friends.length > 0 &&
+          friends.map((friend) => <FriendComponent value={friend.id} key={friend.id} profile={friend} />)}
+      </ToggleButtonGroup>
     </>
   );
 }
