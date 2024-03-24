942effc0aeaa34484533d17c94b78cc6cebc4e84
parkcoool <yickle1345@naver.com>
2024-03-24 16:50:00 +0900

    Feat(friend): Add friend request page

--- src/routers/friend.tsx
+++ src/routers/friend.tsx
@@ -1,11 +1,14 @@
 import type { RouteObject } from "react-router";
 
 import Friend from "pages/main/friend";
+import Request from "pages/main/friend/request";
 
 const friendRouter: RouteObject = {
   path: "/friend",
-  element: <Friend />,
-  children: [],
+  children: [
+    { index: true, element: <Friend /> },
+    { path: "request", element: <Request /> },
+  ],
 };
 
 export default friendRouter;
