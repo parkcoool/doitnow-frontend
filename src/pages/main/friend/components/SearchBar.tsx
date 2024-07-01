666b74d30fd91b5b340e13abe5b798cc24b557e4
parkcool <yickle1345@naver.com>
2024-07-01 11:23:51 +0000

    Feat: Create user search

--- src/pages/main/friend/components/SearchBar.tsx
+++ src/pages/main/friend/components/SearchBar.tsx
@@ -1,12 +1,60 @@
 /** @jsxImportSource @emotion/react */
-import { InputAdornment, TextField } from "@mui/material";
+import React from "react";
+
+import {
+  Autocomplete,
+  CircularProgress,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 
+import searchUser from "apis/searchUser";
 import Narrow from "components/layout/Narrow";
+import useSessionStore from "contexts/useSessionStore";
 
 import type { TextFieldProps } from "@mui/material";
 
+interface UserSearchResult {
+  id: number;
+  name: string;
+  username: string;
+  profileImage: string | null;
+}
+
 export default function SeachBar(props: TextFieldProps) {
+  const session = useSessionStore();
+
+  const [loading, setLoading] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState<UserSearchResult[]>(
+    []
+  );
+
+  React.useEffect(() => {
+    const accessToken = session.accessToken;
+    if (accessToken === null) return;
+
+    if (inputValue.length < 3) {
+      setSearchResults([]);
+      return;
+    }
+
+    let canceled = false;
+    setLoading(true);
+    searchUser({ query: inputValue }, accessToken.token).then((results) => {
+      if (canceled) {
+        return;
+      }
+      setLoading(false);
+      setSearchResults(results.data.users);
+    });
+
+    return () => {
+      canceled = true;
+    };
+  }, [inputValue]);
+
   return (
     <div
       css={{
@@ -17,18 +65,37 @@ export default function SeachBar(props: TextFieldProps) {
       }}
     >
       <Narrow>
-        <TextField
-          {...props}
-          size="small"
-          placeholder="이름, 사용자 이름 또는 이메일을 이용하여 사용자 검색"
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchRoundedIcon />
-              </InputAdornment>
-            ),
-          }}
+        <Autocomplete
+          inputValue={inputValue}
+          onInputChange={(_, value) => setInputValue(value)}
+          options={searchResults}
+          getOptionLabel={(option) => option.name}
+          open={inputValue.length > 2}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              {...props}
+              size="small"
+              placeholder="이름 또는 사용자 이름을 이용하여 사용자 검색"
+              fullWidth
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchRoundedIcon />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <React.Fragment>
+                    {loading ? (
+                      <CircularProgress color="inherit" size={20} />
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
+            />
+          )}
         />
       </Narrow>
     </div>
