3fb7e76b64a1296652042444eb8a97e7494b89fc
parkcool <yickle1345@naver.com>
2024-07-13 07:15:59 +0000

    Feat: Add `Today` component

--- src/pages/main/home/components/Today.tsx
+++ src/pages/main/home/components/Today.tsx
@@ -1,10 +1,81 @@
-import { Paper } from "@mui/material";
+/** @jsxImportSource @emotion/react */
+import React from "react";
 
-interface TodayProps {
-  doneTaskCount: number;
-  totalTaskCount: number;
+import {
+  Button,
+  LinearProgress,
+  Paper,
+  Skeleton,
+  Typography,
+} from "@mui/material";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import styled from "@emotion/styled";
+
+const QuickButton = styled(Button)(({ theme }) => ({
+  borderRadius: 5,
+  padding: "5px 10px",
+  fontSize: 14,
+  fontWeight: 500,
+}));
+
+export interface TodayProps {
+  username?: string;
+  doneTaskCount?: number;
+  totalTaskCount?: number;
 }
 
-export default function Today({ doneTaskCount, totalTaskCount }: TodayProps) {
-  return <Paper></Paper>;
+export default function Today({
+  username,
+  doneTaskCount,
+  totalTaskCount,
+}: TodayProps) {
+  const [message, setMessage] = React.useState<string>();
+
+  const progressValue =
+    totalTaskCount === undefined
+      ? 0
+      : ((doneTaskCount ?? 0) / totalTaskCount) * 100;
+
+  React.useEffect(() => {
+    if (
+      username !== undefined &&
+      doneTaskCount !== undefined &&
+      totalTaskCount !== undefined
+    ) {
+      if (totalTaskCount === 0)
+        setMessage(`${username}님, 오늘 할 일이 없어요! 😊`);
+      else
+        setMessage(
+          `${username}님, 오늘 ${doneTaskCount}개 중 ${totalTaskCount}개 완료했어요!`
+        );
+    }
+  }, [username, doneTaskCount, totalTaskCount]);
+
+  return (
+    <Paper
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        padding: "20px",
+      }}
+    >
+      <Typography variant="h1" fontSize={24} fontWeight={500}>
+        {message ?? <Skeleton width={200} animation="wave" />}
+      </Typography>
+      <LinearProgress
+        variant="determinate"
+        value={progressValue}
+        css={{
+          height: 10,
+          borderRadius: 5,
+        }}
+      />
+      <div>
+        <QuickButton startIcon={<AddRoundedIcon />} color="inherit">
+          할 일 추가하기
+        </QuickButton>
+      </div>
+    </Paper>
+  );
 }
--- src/pages/main/home/index.tsx
+++ src/pages/main/home/index.tsx
@@ -1,15 +1,50 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+
+import getTaskCount from "apis/getTaskCount";
+
 import useSessionStore from "contexts/useSessionStore";
 
 import Narrow from "components/layout/Narrow";
 
 import Today from "./components/Today";
 
+import type { TodayProps } from "./components/Today";
+
 export default function Home() {
   const session = useSessionStore();
 
+  const [todayData, setTodayData] = React.useState<TodayProps>({});
+
+  React.useEffect(() => {
+    // today
+    {
+      if (session.user !== null)
+        setTodayData((prev) => ({ ...prev, username: session.user?.username }));
+
+      if (session.accessToken !== null) {
+        const accessToken = session.accessToken;
+        getTaskCount({ done: true }, accessToken.token).then((res) =>
+          setTodayData((prev) => ({ ...prev, doneTaskCount: res.data.count }))
+        );
+        getTaskCount({ done: false }, accessToken.token).then((res) =>
+          setTodayData((prev) => ({ ...prev, totalTaskCount: res.data.count }))
+        );
+      }
+    }
+  }, [session]);
+
   return (
     <>
-      <Narrow>h</Narrow>
+      <Narrow>
+        <div
+          css={{
+            paddingTop: "20px",
+          }}
+        >
+          <Today {...todayData} />
+        </div>
+      </Narrow>
     </>
   );
 }
